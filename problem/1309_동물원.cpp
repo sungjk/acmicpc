4ffc9e4b7cea0cc10bd0303b8c7365c85e22353a
SungjungKim <ajax0615@gmail.com>
2016-09-01 15:10:28 +0900

    Dynamic Programming

---
+++ problem/1309_동물원.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdio>
+using namespace std;
+
+const int MOD = 9901;
+int zoo[100001];
+
+int main() {
+	int n; scanf("%d", &n);
+	zoo[0] = 1; zoo[1] = 3;
+	for (int i = 2; i <= n; ++i) {
+		zoo[i] = 2 * zoo[i - 1] + zoo[i - 2];
+		zoo[i] %= MOD;
+	}
+	printf("%d\n", zoo[n] % MOD);
+
+	return 0;
+}
