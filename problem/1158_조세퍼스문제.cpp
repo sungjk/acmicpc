1b3248727544a8b61b621fb9c4531d8d9e2e56fb
SungjungKim <ajax0615@gmail.com>
2016-10-04 10:23:02 +0900

    list

---
+++ problem/1158_조세퍼스문제.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+void josephus(int n, int m) {
+	list<int> survivors;
+	for (int i = 1; i <= n; ++i) {
+		survivors.push_back(i);
+	}
+
+	list<int>::iterator kill = survivors.begin();
+	cout << "<";
+	while (survivors.size() > 0) {
+		for (int i = 0; i < m - 1; ++i) {
+			++kill;
+			if (kill == survivors.end()) {
+				kill = survivors.begin();
+			}
+		}
+
+		if (survivors.size() != 1) cout << *kill << ", ";
+		else cout << *kill;
+
+		kill = survivors.erase(kill);
+		if (kill == survivors.end()) {
+			kill = survivors.begin();
+		}
+	}
+	cout << ">" << endl;
+}
+
+int main() {
+	int n, m; cin >> n >> m;
+	josephus(n, m);
+	return 0;
+}
