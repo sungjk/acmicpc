61b5e44b290cb6357d91979132de4c787ff36119
sungjk <ajax0615@gmail.com>
2019-03-11 02:10:08 +0900

    add dfs, bfs problems

--- java/main/java/DFSBFS.java
+++ java/main/java/DFSBFS.java
@@ -28,12 +28,53 @@ public class DFSBFS {
         return plus + minus;
     }
 
+    // https://programmers.co.kr/learn/courses/30/lessons/43162?language=java
+    public static int test2(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for (int current = 0; current < n; current++) {
+            if (!visited[current]) {
+                answer++;
+                rec2(computers, visited, current);
+            }
+        }
+        return answer;
+    }
+
+    public static void rec2(int[][] computers, boolean[] visited, int current) {
+        visited[current] = true;
+        for (int next = 0; next < computers.length; next++) {
+            if (current == next) continue;
+            if (!visited[next] && computers[current][next] == 1) {
+                rec2(computers, visited, next);
+            }
+        }
+    }
+
     public static class UnitTest {
         @Test
         public void _test1() {
             int[] numbers = new int[] { 1, 1, 1, 1, 1 };
             int target = 3;
             assertEquals(test1(numbers, target), 5);
+        }
+
+        @Test
+        public void _test2() {
+            int n1 = 3;
+            int[][] computers1 = new int[][] {
+                { 1, 1, 0 },
+                { 1, 1, 0 },
+                { 0, 0, 1 }
+            };
+            int n2 = 3;
+            int[][] computers2 = new int[][] {
+                { 1, 1, 0 },
+                { 1, 1, 1 },
+                { 0, 1, 1 }
+            };
+            assertEquals(test2(n1, computers1), 2);
+            assertEquals(test2(n2, computers2), 1);
         }
     }
 }
