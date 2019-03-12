18fac7ec74ab12f9e84ff4280018a65118c947c2
sungjk <ajax0615@gmail.com>
2019-03-12 17:44:21 +0900

    add graph problems

--- java/main/java/A1068.java
+++ java/main/java/A1068.java
@@ -1,7 +1,63 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created by jeremy on 03/11/2019.
  */
+/*
+input:
+5
+-1 0 0 1 1
+2
+
+output:
+2
+ */
 public class A1068 {
+
+    public static int[] initTree(Scanner sc, int numOfNodes) {
+        int[] tree = new int[numOfNodes];
+        for (int i = 0; i < numOfNodes; i++) {
+            tree[i] = sc.nextInt();
+        }
+        return tree;
+    }
+
+    private static boolean hasChild(int[] tree, int current, boolean isLeftChild) {
+        if (current > 0 && tree[current] == -1) return false;
+        return tree.length > current * 2 + (isLeftChild ? 1 : 2);
+    }
+
+    public static int countLeafNodes(int[] tree, int current) {
+        if (tree.length == 1) return 0;
+        if (current > 0 && tree[current] == -1) return 0;
+
+        boolean hasLeftChild = hasChild(tree, current, true);
+        boolean hasRightChild = hasChild(tree, current, false);
+        if (!hasLeftChild && !hasRightChild) return 1;
+
+        int count = 0;
+        if (hasLeftChild) {
+            count += countLeafNodes(tree, current * 2 + 1) ;
+        }
+        if (hasRightChild) {
+            count += countLeafNodes(tree, current * 2 + 2);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfNodes = sc.nextInt();
+        // initialize
+        int[] tree = initTree(sc, numOfNodes);
+
+        // delete a node
+        int deleteNode = sc.nextInt();
+        tree[deleteNode] = -1;
+
+        // count the leaf nodes
+        System.out.println(countLeafNodes(tree, 0));
+        sc.close();
+    }
+
 }
--- java/main/java/A5567.java
+++ java/main/java/A5567.java
@@ -1,5 +1,101 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Created by jeremy on 03/12/2019.
  */
+/*
+input:
+6
+5
+1 2
+1 3
+3 4
+2 3
+4 5
+
+3
+1
+2 3
+
+2
+1
+1 2
+
+3
+2
+2 1
+3 2
+
+10
+10
+4 10
+7 10
+3 7
+6 8
+1 7
+1 6
+1 5
+1 9
+2 4
+3 8
+
+output:
+3
+0
+1
+2
+7
+ */
 public class A5567 {
+    public static int invite(boolean[][] relations, int n) {
+        boolean[][] visited = new boolean[n + 1][n + 1];
+
+        // 1촌만 초기화
+        List<Integer> ilchons = IntStream.range(2, n + 1)
+            .boxed()
+            .filter(person -> {
+                if (relations[1][person]) {
+                    visited[1][person] = true;
+                    visited[person][1] = true;
+                    return true;
+                }
+                return false;
+            })
+            .collect(Collectors.toList());
+
+        return IntStream.range(2, n + 1)
+            .boxed()
+            .filter(person -> {
+                for (int ilchon : ilchons) {
+                    if (ilchon == person) return true;
+                    // 상근이와 1촌X && 순회X && 나와 1촌인 경우
+                    if (!ilchons.contains(person) && !visited[person][ilchon] && relations[person][ilchon]) {
+                        visited[person][ilchon] = true;
+                        visited[ilchon][person] = true;
+                        return true;
+                    }
+                }
+                return false;
+            })
+            .collect(Collectors.toList())
+            .size();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        boolean[][] relations = new boolean[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            int person1 = sc.nextInt();
+            int person2 = sc.nextInt();
+            relations[person1][person2] = true;
+            relations[person2][person1] = true;
+        }
+
+        System.out.println(invite(relations, n));
+    }
 }
