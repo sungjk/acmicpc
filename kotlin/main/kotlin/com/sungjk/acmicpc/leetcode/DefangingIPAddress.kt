68374bc7613e6ad0a5d0f25fa6172451e90dd6cb
sungjk <ajax0615@gmail.com>
2019-10-12 15:25:48 +0900

    Add DefangingIPAddress

--- kotlin/main/kotlin/com/sungjk/acmicpc/leetcode/DefangingIPAddress.kt
+++ kotlin/main/kotlin/com/sungjk/acmicpc/leetcode/DefangingIPAddress.kt
@@ -1,5 +1,23 @@
 package com.sungjk.acmicpc.leetcode
 
+import org.junit.Assert
+import org.junit.Test
+
 /**
  * Created by jeremy on 2019/10/12.
  */
+class DefangingIPAddress {
+    fun defangIPaddr(address: String): String {
+        return address.replace(".", "[.]")
+    }
+
+    @Test
+    fun test1() {
+        Assert.assertEquals("1[.]1[.]1[.]1", defangIPaddr("1.1.1.1"))
+    }
+
+    @Test
+    fun test2() {
+        Assert.assertEquals("255[.]100[.]50[.]0", defangIPaddr("255.100.50.0"))
+    }
+}
